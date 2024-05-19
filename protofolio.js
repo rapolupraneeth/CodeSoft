eb33d35b7787d541414df0720f023fe8c8444194
rapolupraneeth <155894161+rapolupraneeth@users.noreply.github.com>
2024-05-19 12:04:38 +0530

    Add files via upload

---
+++ protofolio.js
@@ -0,0 +1,6 @@
+// This script can be used to add interactivity in the future
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+});
+document.getElementById('year').textContent = new Date().getFullYear();
+   
